a9449ffc55efe901415e1d7af90508b7bc731428
远鹏 张 <zhyp1946770956@163.com>
2025-03-20 22:58:35 +0800

    添加了.vscode 的配置文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,30 @@
 #include "opencv2/opencv.hpp"
-#include "opencv2/highgui.hpp"
 #include <vector>
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 using namespace cv;
-/* Global Value */ 
+/* Global Value */
 vector<Point2i> points;
 string imgPath = "test1.jpg";
 Mat img = imread(imgPath);
 int HEIGHT = 9;
 int WIDTH = 9;
 
-void fastCornerDetection(Point& pt)
+void fastCornerDetection(Point &pt)
 {
     // 0. 截取click点附近的图像
-    Rect roi(pt.x - HEIGHT/2, pt.y - WIDTH/2, HEIGHT, WIDTH);
+    Rect roi(pt.x - HEIGHT / 2, pt.y - WIDTH / 2, HEIGHT, WIDTH);
     Mat image = img(roi);
     // 1. 初步处理数据
     Mat gray;
 
     cvtColor(image, gray, COLOR_BGR2GRAY);
-    //imshow("Gray", gray);
-    GaussianBlur(gray,gray,Size(3,3),1.5);
-    //imshow("Bin_Gray",gray);
-    //preprocessImg(gray);
+    // imshow("Gray", gray);
+    GaussianBlur(gray, gray, Size(3, 3), 1.5);
+    // imshow("Bin_Gray",gray);
+    // preprocessImg(gray);
 
     // 2. 创建 FAST 检测器
     Ptr<FastFeatureDetector> fast = FastFeatureDetector::create();
@@ -39,71 +38,67 @@ void fastCornerDetection(Point& pt)
     vector<KeyPoint> keypoints;
     fast->detect(gray, keypoints);
 
-    if(keypoints.size() !=0)
-        pt = Point(pt.x+keypoints[0].pt.x-HEIGHT/2-1,pt.y+keypoints[0].pt.y-WIDTH/2-1);
+    if (keypoints.size() != 0)
+        pt = Point(pt.x + keypoints[0].pt.x - HEIGHT / 2 - 1, pt.y + keypoints[0].pt.y - WIDTH / 2 - 1);
     // 5. 绘制角点
-    //drawKeypoints(image, keypoints, image, Scalar(0, 255, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    
+    // drawKeypoints(image, keypoints, image, Scalar(0, 255, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 }
 
 /* Function */
 double getAngel()
 {
-    Vec2d p1 = {(points[2].x-points[1].x)*1.0,(points[2].y-points[1].y)*1.0};
-    Vec2d p2 = {(points[0].x-points[1].x)*1.0,(points[0].y-points[1].y)*1.0};
+    // 计算两个向量的夹角
+    Vec2d p1 = {(points[2].x - points[1].x) * 1.0, (points[2].y - points[1].y) * 1.0};
+    Vec2d p2 = {(points[0].x - points[1].x) * 1.0, (points[0].y - points[1].y) * 1.0};
 
-    double angleR = acosf(p1.dot(p2)/norm(p1)/norm(p2));
-    double angleD = angleR/3.1415*180;
+    double angleR = acosf(p1.dot(p2) / norm(p1) / norm(p2)); // 计算两个向量的弧度夹角
+    double angleD = angleR / 3.1415 * 180;                   // 弧度转角度
     return angleD;
-
 }
 
-void onMouse(int event, int x, int y, int flag, void* param)
+/* Mouse Event */
+void onMouse(int event, int x, int y, int flag, void *param)
 {
-    Point click_pt = {x,y};
+    Point click_pt = {x, y};
 
-    if(event == EVENT_LBUTTONUP)
+    if (event == EVENT_LBUTTONUP)
     {
-        fastCornerDetection(click_pt);
+        fastCornerDetection(click_pt); // 快速角点检测
         points.push_back(click_pt);
-        circle(img,click_pt,2,Scalar(0,0,255),2,8,0);
-        cout<<points<<endl;
-        
-        if(points.size() == 1)
+        circle(img, click_pt, 2, Scalar(0, 0, 255), 2, 8, 0);
+        cout << points << endl;
+
+        if (points.size() == 1)
             return;
-        line(img,points[points.size()-2],points.back(),Scalar(0,255,0), 1, 8,0);
+        line(img, points[points.size() - 2], points.back(), Scalar(0, 255, 0), 1, 8, 0);
         // 当有三个点时
         // 计算角度、清空列表
-        if(points.size() == 3)
+        if (points.size() == 3)
         {
             double angleR = getAngel();
-            cout << "angle = "<<angleR<< endl;
+            cout << "angle = " << angleR << endl;
             points.clear();
         }
         // 不到三个点时
-        
-        
     }
 }
 
-
-
 int main(int argc, char const *argv[])
 {
-    
+
     namedWindow("Res", WINDOW_NORMAL);
 
-    while(true)
+    while (true)
     {
-        imshow("Res",img);
-        setMouseCallback("Res",onMouse);
+        imshow("Res", img);
+        setMouseCallback("Res", onMouse);
 
-        //out
+        // out
         int Key = waitKey(1);
-        if(Key == 27)
+        if (Key == 27)
             break;
         // clear sceen
-        if(Key == 'c')
+        if (Key == 'c')
         {
             img = imread(imgPath);
             points.clear();
